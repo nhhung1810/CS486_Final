68054994102c7dc07ebaa3ac4c04d47d550dd76b
Bacon <nguyenlamtuong2001@gmail.com>
2021-08-14 09:16:15 +0700

    change

--- SPV202_CS486_Team11/SPV202_CS486_Team11/Program.cs
+++ SPV202_CS486_Team11/SPV202_CS486_Team11/Program.cs
@@ -16,7 +16,7 @@ namespace SPV202_CS486_Team11
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new Form1());
         }
     }
 }
