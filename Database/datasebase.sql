d13ea850709526382dbc6d825f8b162f26b51f67
Bùi Quang Minh <minhdeptrai2709@gmail.com>
2021-08-14 10:01:14 +0700

    done sql

--- Database/datasebase.sql
+++ Database/datasebase.sql
@@ -7,7 +7,8 @@ go
 
 create table Singer(
     id int not null PRIMARY KEY,
-    name NVARCHAR(100) NOT NULL
+    name NVARCHAR(100) NOT NULL,
+	isOfficial int CHECK(isOfficial in (1, 0))
 )
 
 go
@@ -29,10 +30,7 @@ create table Interview(
 )
 
 --
-create table official(
-    singerid int not null REFERENCES singer(id),
-    isOfficial int CHECK(isOfficial in (1, 0))
-)
+
 
 go
 
@@ -46,3 +44,93 @@ use master
 go
 drop DATABASE CS486_team11_DB
 
+CREATE OR ALTER PROCEDURE addInterview
+@official int,
+@reserve int,
+@score int
+
+AS
+BEGIN TRANSACTION
+BEGIN TRY
+	--IF LEGIT INSERT
+	--ELSE THROW
+
+	IF NOT EXISTS ( SELECT *
+					FROM Singer
+					WHERE id = @official AND isOfficial = 1)
+		THROW 50000, 'official not exist', 1;
+
+	IF NOT EXISTS ( SELECT *
+					FROM Singer
+					WHERE id = @reserve AND isOfficial = 0)
+		THROW 50000, 'reserve not exist', 1;
+
+	IF ((SELECT COUNT (*)
+				FROM Interview
+				WHERE interviewer = @official) > 0) 
+		THROW 50000, '1 participant must only interview 1 member', 1;
+
+	IF ((SELECT COUNT (*)
+				FROM Interview
+				WHERE interviewer = @reserve) > 0) 
+		THROW 50000, '1 participant must only interview 1 member', 1;
+
+
+	INSERT INTO Interview (interviewer, interviewee, score) 
+					VALUES (@official, @reserve, @score);
+
+	COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION;
+	THROW;
+END CATCH;
+GO
+
+CREATE OR ALTER PROCEDURE addDuet
+@official int,
+@reserve int,
+@songid int
+
+AS
+BEGIN TRANSACTION
+BEGIN TRY
+	--IF LEGIT INSERT
+	--ELSE THROW
+	IF NOT EXISTS ( SELECT *
+					FROM Singer
+					WHERE id = @official AND isOfficial = 1)
+		THROW 50000, 'official not exist', 1;
+
+	IF NOT EXISTS ( SELECT *
+					FROM Singer
+					WHERE id = @reserve AND isOfficial = 0)
+		THROW 50000, 'reserve not exist', 1;
+
+	IF NOT EXISTS ( SELECT *
+					FROM Song
+					WHERE id = @songid)
+		THROW 50000, 'song not exist', 1;
+
+	IF ((SELECT COUNT (*)
+				FROM performance
+				WHERE singerid = @official) > 0) 
+		THROW 50000, '1 participant must only sing 1 song', 1;
+
+	IF ((SELECT COUNT (*)
+				FROM performance
+				WHERE singerid = @reserve) > 0) 
+		THROW 50000, '1 participant must only sing 1 song', 1;
+
+
+	INSERT INTO performance (singerid, songid) 
+			VALUES	(@official, @songid), 
+					(@reserve, @songid);
+
+	COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION;
+	THROW;
+END CATCH;
+GO
